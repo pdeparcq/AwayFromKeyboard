fee5e220c504370c4f65fcdf934e7cf88839f9c0
Pieter Deparcq <Pieter@hellocustomer.com>
2020-12-13 18:02:56 +0100

    First working api

---
+++ AwayFromKeyboard.Api/Controllers/ModulesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AwayFromKeyboard.Domain.Meta;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwayFromKeyboard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ModulesController : Controller
+    {
+        private readonly MetaDbContext _metaDbContext;
+
+        public ModulesController(MetaDbContext metaDbContext)
+        {
+            _metaDbContext = metaDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Module>> Get()
+        {
+            return await _metaDbContext.Modules.Where(m => m.ParentModule == null).ToListAsync();
+        }
+    }
+}
