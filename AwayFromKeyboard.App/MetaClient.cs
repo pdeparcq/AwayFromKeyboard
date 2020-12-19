9a014ed1bec8c68a833c11d6dbdf855b8d73d3a3
Pieter Deparcq <Pieter@hellocustomer.com>
2020-12-19 17:53:58 +0100

    Blazor forms and navigation

--- AwayFromKeyboard.App/MetaClient.cs
+++ AwayFromKeyboard.App/MetaClient.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace AwayFromKeyboard.App
@@ -9,10 +12,22 @@ namespace AwayFromKeyboard.App
         {
             return await ModulesAllAsync();
         }
+
+        public async Task<Module> GetModule(Guid moduleId)
+        {
+            return (await ModulesAllAsync()).Single(m => m.Id == moduleId);
+        }
+
+        public async Task AddEntity(CreateType model)
+        {
+            await Entities3Async(model);
+        }
     }
 
     public interface IMetaClient
     {
         Task<IEnumerable<Module>> GetAllModules();
+        Task<Module> GetModule(Guid moduleId);
+        Task AddEntity(CreateType model);
     }
 }
