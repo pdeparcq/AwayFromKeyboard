38fa94a06a40da38e8f2cb2594412cb5e4c8441c
Pieter Deparcq <Pieter@hellocustomer.com>
2020-12-19 09:28:09 +0100

    First working blazor app

---
+++ AwayFromKeyboard.App/MetaClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AwayFromKeyboard.App
+{
+    public partial class MetaClient : IMetaClient
+    {
+        public async Task<IEnumerable<Module>> GetAllModules()
+        {
+            return await ModulesAllAsync();
+        }
+    }
+
+    public interface IMetaClient
+    {
+        Task<IEnumerable<Module>> GetAllModules();
+    }
+}
